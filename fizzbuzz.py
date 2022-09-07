aa2e1b13ab20351d43e42e78948507f6357931ff
satoridev01 <89515805+satoridev01@users.noreply.github.com>
2022-09-07 16:20:23 -0300

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+import sys
+
+def fizzbuzz(n)
+    if n % 15 == 0:
+        return 'FizzBuzz'
+    elif n % 3 == 0:
+        return 'Fizz'
+    elif n % 5 == 0:
+        return 'Buzz'
+    else:
+        return str(n)
+
+if len(sys.argv) == 1:
+    fizzbuzz(sys.argv[1])
